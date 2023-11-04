531e01f0a23e26e670b720741558508785c7eb65
Emran <emranovin@gmail.com>
2023-11-04 19:59:02 +0330

    Enhance XCCONFIG per scheme

--- App/AppDelegate.swift
+++ App/AppDelegate.swift
@@ -10,21 +10,21 @@ import OneSignalFramework
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-       // Remove this method to stop OneSignal Debugging
-       OneSignal.Debug.setLogLevel(.LL_VERBOSE)
+        // Remove this method to stop OneSignal Debugging
+        OneSignal.Debug.setLogLevel(.LL_VERBOSE)
         
-       // OneSignal initialization
-        OneSignal.initialize(<#String#>, withLaunchOptions: launchOptions)
-
-       // requestPermission will show the native iOS notification permission prompt.
-       // We recommend removing the following code and instead using an In-App Message to prompt for notification permission
-       OneSignal.Notifications.requestPermission({ accepted in
-         print("User accepted notifications: \(accepted)")
-       }, fallbackToSettings: true)
-
-       // Login your customer with externalId
-       // OneSignal.login("EXTERNAL_ID")
-            
-       return true
+        // OneSignal initialization
+        OneSignal.initialize(oneSignalAppID, withLaunchOptions: launchOptions)
+        
+        // requestPermission will show the native iOS notification permission prompt.
+        // We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+        OneSignal.Notifications.requestPermission({ accepted in
+            print("User accepted notifications: \(accepted)")
+        }, fallbackToSettings: true)
+        
+        // Login your customer with externalId
+        // OneSignal.login("EXTERNAL_ID")
+        
+        return true
     }
 }
---
+++ App/Config.swift
@@ -0,0 +1,16 @@
+//
+//  Config.swift
+//  Sibaro
+//
+//  Created by Emran on 11/4/23.
+//
+
+import Foundation
+
+var isDebug: Bool {
+    return Bundle.main.info(for: "IsDebug") == "true" ? true : false
+}
+
+var oneSignalAppID: String {
+    return Bundle.main.info(for: "OneSignalAppID")
+}
